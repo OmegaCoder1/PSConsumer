868f12ec123835836e239f79c578b7c4f1fb8901
larsid <larsid@lvm07>
2023-02-23 17:07:33 -0300

    Server teste

---
+++ PSConsumer/src/main/java/uefs/ecomp/PSConsumer/Controller/Server.java
@@ -0,0 +1,47 @@
+package uefs.ecomp.PSConsumer.Controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author larsid
+ */
+public class Server {
+    private int a;
+    
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(4000);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String greeting = in.readLine();
+            if ("hello server".equals(greeting)) {
+                out.println("hello client");
+            }
+            else {
+                out.println("unrecognised greeting");
+            }
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+    public static void main(String[] args) throws IOException {
+        Server server=new Server();
+        server.start(4000);
+    }
+}
+
