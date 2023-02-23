8d69757e85e26514bb5c1c5615cb2ae87ee53e54
larsid <larsid@lvm07>
2023-02-23 16:48:22 -0300

    Controller update

--- PSConsumer/src/main/java/uefs/ecomp/PSConsumer/Controller/Controller.java
+++ PSConsumer/src/main/java/uefs/ecomp/PSConsumer/Controller/Controller.java
@@ -1,9 +1,16 @@
 package uefs.ecomp.PSConsumer.Controller;
 
+import java.util.List;
+import uefs.ecomp.PSConsumer.Model.Medidor;
+
 /**
  *
  * @author Robson
  */
 public class Controller {
+    private List<Medidor> consumo;
     
+    public void Teste(){
+        
+    }
 }
